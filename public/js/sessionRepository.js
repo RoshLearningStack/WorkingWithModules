26c2b35cf08cb00e46c8ce1bac42294eb87841d0
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-26 18:35:06 +1200

    modularizing with named/default exports

---
+++ public/js/sessionRepository.js
@@ -0,0 +1,26 @@
+
+const sessionURL = 'sessions.json';
+let sessionList = [];
+
+// default export
+// export default 
+function getSessions() {
+    return new Promise(function (resolve, reject) {
+        var oReq = new XMLHttpRequest();
+        oReq.onload = function (e) {
+            sessionList = e.target.response;
+            resolve(sessionList);
+        };
+        oReq.open('GET', sessionURL, true);
+        oReq.responseType = 'json';
+        oReq.send();
+    })
+}
+
+// named export
+// export {getSessions as sessions};
+
+ export {// default export 
+    getSessions as default, 
+    // named export
+    sessionURL}
