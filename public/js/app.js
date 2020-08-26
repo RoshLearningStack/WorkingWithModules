d6ae01b48de2b8888378282af0b2eaf57c789368
Roshan Deniyage <crdeniyage@gmail.com>
2020-08-26 18:51:25 +1200

    Named import individually

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,12 @@
 // default import
 import getSessions from './sessionRepository.js';
 // named import
-import {sessionTemplate, errorMessage} from './template.js';
+import * as template from './template.js';
 
 function render() {
     var list = document.querySelector('#sessions');
     if (!list) return;
-    list.innerHTML = sessionTemplate(data.listItems);
+    list.innerHTML = template.sessionTemplate(data.listItems);
 };
 
 var data = {
